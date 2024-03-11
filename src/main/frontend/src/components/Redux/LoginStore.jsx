e1ce20812930cfe1cbddae38ec67aa107c38e3ed
qazplm5602 <t01048187045@gmail.com>
2024-03-11 19:22:50 +0900

    로그인 실시간 업데이트트트

--- src/main/frontend/src/components/Redux/LoginStore.jsx
+++ src/main/frontend/src/components/Redux/LoginStore.jsx
@@ -1,15 +1,27 @@
 const defaultStore = {
     logined: false,
+    loading: false,
     id: "test",
     name: "domi",
 }
 
 export default function(store = defaultStore, action) {
-    if (action !== "login.update") // 그냥 데이터만 줭
+    if (action.type !== "login.update") // 그냥 데이터만 줭
         return store;
 
     const newStore = {...store}; // 불변성 ㅁㄴㅇㄹ
-    
+    newStore.loading = action.load;
+    if (!action.load) { // 읭 로딩중이 아님
+        if (action.data === undefined || !action.data.result) { // 로그인 실패
+            newStore.logined = false;
+            newStore.id = null;
+            newStore.name = null;
+        } else {
+            newStore.logined = true;
+            newStore.id = action.data.id;
+            newStore.name = action.data.name;
+        }
+    }
     
     return newStore;
 }
