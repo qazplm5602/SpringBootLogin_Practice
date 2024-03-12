06fb3e3fe5a9d67a8610d35cd9c3e083907a2678
qazplm5602 <t01048187045@gmail.com>
2024-03-12 09:01:51 +0900

    재활용 긱기기

--- src/main/frontend/src/components/Login/Login.jsx
+++ src/main/frontend/src/components/Login/Login.jsx
@@ -1,8 +1,11 @@
 import Background from "../Recycle/BackgroundBox";
+import Section from "../Recycle/Section";
 import style from './login.module.css';
 
 export default function Login() {
     return <Background className={style.background}>
-        
+        <Section title={"로그인"} isContainer={true} className={style.loginSection}>
+            
+        </Section>
     </Background>;
 }
--- src/main/frontend/src/components/Recycle/Section.jsx
+++ src/main/frontend/src/components/Recycle/Section.jsx
@@ -1,8 +1,9 @@
 // ♻️♻️♻️♻️♻️♻️
 
-export default function Section({title, children, ...props}) {
+export default function Section({title, children, isContainer, ...props}) {
     return <section {...props}>
-        <h1>{title}</h1>
-        {children}
+        {title && <h1>{title}</h1>}
+        {/* 🌱 main 이라는 태그 처음 암 ㅋㅋㅋㅋ 🌱 */}
+        {isContainer ? <main>{children}</main> : children}
     </section>;
 }
