fa2756eab90330a772ec08550f7223bed741e8e1
qazplm5602 <t01048187045@gmail.com>
2024-03-14 12:31:41 +0900

    로그인 화면 이펙트

--- src/main/frontend/src/components/Index/Index.jsx
+++ src/main/frontend/src/components/Index/Index.jsx
@@ -6,9 +6,12 @@ import Button from "../Recycle/Button";
 import { Link } from "react-router-dom";
 import Section from "../Recycle/Section";
 import Input from "../Recycle/Input";
+import { useEffect, useRef, useState } from "react";
 
 export default function Index() {
     const login = useSelector(value => value.login);
+    const [loginShow, SetLoginShow] = useState(false);
+
     const textClass = [style.welcome];
     if (!login.load) {
         textClass.push(login.logined ? style.login : style.logout);
@@ -22,16 +25,43 @@ export default function Index() {
             {(!login.load) && (login.logined ? <>Welcome! <span className={style.name}>{login.name}</span> user</> : "Please, Login U!")}
         </span>
 
-        <Link style={{textDecorationLine: "none"}} to={"/login"}>
-            <Button text={"로그인"} className={style.loginBtn} style={{display: "block"}} />
-        </Link>
+        {/* <Link style={{textDecorationLine: "none"}} to={"/login"}> */}
+            <Button text={"로그인"} className={style.loginBtn} style={{display: "block"}} onClick={() => SetLoginShow(true)} />
+        {/* </Link> */}
 
-        <LoginBox />
+        <LoginBox showState={[loginShow, SetLoginShow]} />
     </Background>;
 }
 
-function LoginBox() {
-    return <Background className={[loginStyle.background].join(" ")}>
+function LoginBox({showState}) {
+    const waitHandler = useRef();
+    const [none, setNone] = useState(!showState[0]);
+    const [show, setShow] = useState(showState[0]);
+
+    const bgClass = [loginStyle.background];
+    if (!show)
+        bgClass.push(loginStyle.hide);
+
+    useEffect(() => {
+        if (waitHandler.current)
+            clearTimeout(waitHandler.current);
+
+        if (showState[0]) {
+            setNone(false);
+            waitHandler.current = setTimeout(() => {
+                setShow(true);
+            }, 10);
+        } else {
+            setShow(false);
+            waitHandler.current = setTimeout(() => {
+                setNone(true);
+            }, 250);
+        }
+    }, [showState[0]]);
+
+    if (none) return;
+
+    return <Background className={bgClass.join(" ")}>
         <Section title="로그인" isContainer={true} className={loginStyle.loginSection}>
             <Input className={loginStyle.inputContainer} title="아이디" placeholder="아이디를 입력해주세요." type="text" />
             <Input className={loginStyle.inputContainer} title="비밀번호" placeholder="비밀번호를 입력해주세요." type="password" />
